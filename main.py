5b8aa62bdd53563bd54e2f45b64ab63d63d8c076
Proxypro2012 <kabir.tiwari@outlook.com>
2025-06-15 17:12:10 -0400

    NEW 16

--- main.py
+++ main.py
@@ -9,6 +9,26 @@ INDIVIDUALS = [
     "Nandini"
 ]
 
+
+class GetUserSynthesis():
+    def __init__(self, users):
+        self.user = users
+
+    def get_users(self):
+        if self.user not in INDIVIDUALS:
+            raise ValueError(f"User {self.user} is not in the list of individuals: {INDIVIDUALS}")
+        if self.user == "Kabir":
+            return "Kabir created an application that solved a real-world issue at the age of 22. " \
+            "His profits soared for the first few years, but then dipped down suddenly as he lost some of his user base." \
+            "A new sofware update more geared towards the user experience of the application lead him to a steady and marginally " \
+            "increasing income for the next few years"
+    
+
+
+
+
+
+
 st.title("Age to Salary Scenario Prediction")
 
 with st.container(border=True):
@@ -16,7 +36,7 @@ with st.container(border=True):
 
 tab1, tab2 = st.tabs(["Chart", "Dataframe"])
 
-# Ensure dataset is downloaded before loading CSV
+
 kagglehub.dataset_download("codebreaker619/salary-data-with-age-and-experience")
 
 try:
@@ -48,6 +68,11 @@ with tab1:
             title="Age vs Salary by User"
         )
         st.altair_chart(chart, use_container_width=True)
+
+        UserSynthesis = GetUserSynthesis(users="Kabir")
+        st.markdown(
+            f"### User Synthesis for Kabir:\n{UserSynthesis.get_users()}"
+        )
 
 with tab2:
     st.dataframe(filtered_df, use_container_width=True)
