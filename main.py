c65f730f2e74684a31d668f95508fa75dd4eee6a
Proxypro2012 <kabir.tiwari@outlook.com>
2025-06-15 16:31:39 -0400

    NEW 4

--- main.py
+++ main.py
@@ -2,14 +2,19 @@ import pandas as pd
 import streamlit as st
 import kagglehub
 
-r1col1, r1col2, r1col3 = st.columns([1, 2, 1])
+INDIVIDUALS = [
+    "Kabir",
+    "Sanjit",
+    "Nandini"
+]
 
-with r1col1:
-    st.title("Age to salary scenario prediction")
 
-st.divider()
+st.title("Age to salary scenario prediction")
 
-tab1, tab2 = st.tabs(["Kabir", "Sanjit", "Nandini"])
+with st.container(border=True):
+    users = st.multiselect("Users", INDIVIDUALS, default=INDIVIDUALS)
+
+tab1, tab2 = st.tabs(["Kabir", "Dataframe"])
 
 
 with tab1:
@@ -27,5 +32,18 @@ with tab1:
             "Salary": [10000, 15000, 25000, 30000, 50000, 60000, 70000, 80000, 90000, 100000, 120000]
         })
     )
+
+with tab2:
+    df = kagglehub.load_dataset("codebreaker619/salary-data-with-age-and-experience", path="salary_data.csv")
+    st.dataframe(df, use_container_width=True)
+
+    st.download_button(
+        label="Download Data",
+        data=df.to_csv(index=False),
+        file_name="salary_data.csv",
+        mime="text/csv"
+    )
+
+    st.write("Data Source: [KaggleHub](https://www.kaggle.com/datasets/codebreaker619/salary-data-with-age-and-experience/data")
 
 
